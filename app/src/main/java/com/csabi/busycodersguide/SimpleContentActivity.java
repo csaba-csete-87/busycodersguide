0c0276e177ca1e004545ee2e0579ed28306e2f10
Csaba Csete <csaba.csete@nordlogic.com>
2015-01-24 20:24:02 +0200

    Tutorials 1-10

---
+++ app/src/main/java/com/csabi/busycodersguide/SimpleContentActivity.java
@@ -0,0 +1,16 @@
+package com.csabi.busycodersguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SimpleContentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
